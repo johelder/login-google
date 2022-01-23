de01d4c565a1125619be460fd077038ef0f2a383
johelder <johelder.arj@gmail.com>
2022-01-23 18:37:25 -0300

    feat: create login screen

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,69 @@
+import React, {useCallback} from 'react';
+
+import {useDispatch} from 'react-redux';
+import {GoogleLogin} from '../../store/modules/user/actions';
+import {IUser} from '../../store/modules/user/types';
+
+import {Google} from '../../services/google';
+
+import FoxSvg from '../../assets/fox.svg';
+import GoogleIcon from '../../assets/google-icon.svg';
+
+import * as S from './styles';
+import {Alert} from 'react-native';
+
+export const Login = () => {
+  const dispatch = useDispatch();
+
+  const handleSignIn = useCallback(async () => {
+    const response = await Google.signIn();
+
+    if (!response.ok) {
+      return;
+    }
+
+    const user = response.data as IUser;
+
+    dispatch(GoogleLogin(user));
+  }, [dispatch]);
+
+  return (
+    <S.Container>
+      <S.Greetings>
+        <S.Slogan>
+          <S.Title>Fox</S.Title>
+          <S.SubTitle>learn</S.SubTitle>
+        </S.Slogan>
+
+        <S.Description>
+          Welcome to new platform of {'\n'} e-learning. Let’s learn.
+        </S.Description>
+      </S.Greetings>
+
+      <S.FoxContainer>
+        <FoxSvg />
+      </S.FoxContainer>
+
+      <S.Content>
+        <S.SignInButtonWrapper>
+          <S.SignInButton onPress={handleSignIn}>
+            <S.IconWrapper>
+              <GoogleIcon width={35} />
+            </S.IconWrapper>
+
+            <S.SignInTextWrapper>
+              <S.SignInText>Sign in with Google</S.SignInText>
+            </S.SignInTextWrapper>
+          </S.SignInButton>
+        </S.SignInButtonWrapper>
+
+        <S.SignUpButtonWrapper>
+          <S.SignUpButton
+            onPress={() => Alert.alert('Ops!', 'Still under construction...')}>
+            <S.SignUpText>Create an account</S.SignUpText>
+          </S.SignUpButton>
+        </S.SignUpButtonWrapper>
+      </S.Content>
+    </S.Container>
+  );
+};
