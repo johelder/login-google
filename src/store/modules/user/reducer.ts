800ad97f6ccb57cb70c1ae8e664765fe7e08094c
johelder <johelder.arj@gmail.com>
2022-01-23 20:01:16 -0300

    feat: adding user actions to reducer

--- src/store/modules/user/reducer.ts
+++ src/store/modules/user/reducer.ts
@@ -1,10 +1,20 @@
 import {Reducer} from 'redux';
-import {IUserState} from './types';
+import {IUserState, IUser} from './types';
 
-const INITIAL_STATE: IUserState = {} as IUserState;
+const INITIAL_STATE = {} as IUserState;
 
-const user: Reducer<IUserState> = () => {
-  return INITIAL_STATE;
+const user: Reducer<IUserState> = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case 'GOOGLE_LOGIN': {
+      return (state = action.payload);
+    }
+    case 'GOOGLE_LOGOUT': {
+      return (state = {user: {} as IUser});
+    }
+
+    default:
+      return state;
+  }
 };
 
 export default user;
